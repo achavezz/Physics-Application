daae00eedc91e6c8267bde2938582c7d035c84cf
Alex Chavez <achavez@NDF9YXVPDX.local>
2024-02-21 13:06:46 -0600

    Created the various htmls for both motions, newtons, and work/energy

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/linear_motion')
+def linear_motion():
+    return render_template('linear_motion.html')
+
+@app.route('/2d_motion')
+def linear_motion():
+    return render_template('2d_motion.html')
+
+@app.route('/newtons_laws')
+def newtons_laws():
+    return render_template('newtons_laws')
+
+@app.route('/forces')
+def forces():
+    return render_template('forces.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
