aa90cbb34edcff0006363ec8339f26bceb10bc3d
Alex Chavez <achavez@NDF9YXVPDX.local>
2024-02-21 13:12:03 -0600

    Updated functionality to move to newtons and work/energy pages

--- app.py
+++ app.py
@@ -11,16 +11,20 @@ def linear_motion():
     return render_template('linear_motion.html')
 
 @app.route('/2d_motion')
-def linear_motion():
+def twod_motion():
     return render_template('2d_motion.html')
 
 @app.route('/newtons_laws')
 def newtons_laws():
-    return render_template('newtons_laws')
+    return render_template('newtons_laws.html')
 
 @app.route('/forces')
 def forces():
     return render_template('forces.html')
+
+@app.route('/work_energy')
+def work_energy():
+    return render_template('work_energy.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
